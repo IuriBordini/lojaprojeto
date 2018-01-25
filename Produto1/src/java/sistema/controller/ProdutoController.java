a6220cdf994d671d8af1408c047ffdb67c87d34b
Aluno <Aluno@DSN193906>
2018-01-25 21:27:10 -0200

    atualização 3

--- Produto1/src/java/sistema/controller/ProdutoController.java
+++ Produto1/src/java/sistema/controller/ProdutoController.java
@@ -1,26 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sistema.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sistema.modelo.ProdutoDAO;
+import org.springframework.web.bind.annotation.RequestMethod;
+import sistema.modelo.Produto;
 
-/**
- *
- * @author Aluno
- */
+import sistema.modelo.ProdutoDao;
+
+
 @Controller
 @RequestMapping("/produto")
 public class ProdutoController {
-    @RequestMapping("/")
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String produtos(Model model){
-        ProdutoDAO dao = new ProdutoDAO();
+        ProdutoDao dao = new ProdutoDao();
         model.addAttribute("dados", dao.getProdutos());
         return "produtos";
+    }
+    @RequestMapping (value = "/(id)", method = RequestMethod.GET)
+    public String produto (@PathVariable long id, Model model){
+        ProdutoDao dao = new ProdutoDao();
+        Produto produto = dao.produtoId(id);
+        model.addAttribute("produto",produto);
+        return "produto";
     }
 }
